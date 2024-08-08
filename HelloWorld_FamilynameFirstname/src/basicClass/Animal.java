acacc6bdd4c386d8e68db6476aca1dc62c1f3d74
nitta-hiroki <hrk99@192.168.0.183>
2024-08-09 00:32:30 +0900

    課題19修正完了致しました。

--- HelloWorld_FamilynameFirstname/src/basicClass/Animal.java
+++ HelloWorld_FamilynameFirstname/src/basicClass/Animal.java
@@ -7,8 +7,8 @@ public class Animal {
 		System.out.println("動物の名前: " + dog1.animalname); // "犬" が出力されます
 
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力
-		Dog dog2 = new Dog("Max", 5); // 名前を引数で指定、犬の数を5に設定
-		System.out.println("動物の数: " + dog2.animals); // static 変数のため、クラス名を使って呼び出す
+		Dog dog2 = new Dog("MAX", 5); // 名前を引数で指定、犬の数を5に設定
+		System.out.println("動物の数: " + dog2.animals);
 
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力
 		System.out.println(
